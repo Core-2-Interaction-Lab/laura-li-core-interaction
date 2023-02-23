9927c8ae3e8bd177978c42984f04c9bfcf333d49
layurathe <laura.li_li@outlook.com>
2023-02-23 03:06:03 -0500

    project1 and mainpage
    homepage almost done

--- project1/homepage/main.js
+++ project1/homepage/main.js
@@ -1,17 +1,37 @@
-var box = document.querySelector('.cube');
-var radioGroup = document.querySelector('.radio-group');
-var currentClass = '';
+console.log("hello"); 
 
-function changeSide() {
-  var checkedRadio = radioGroup.querySelector(':checked');
-  var showClass = 'show-' + checkedRadio.value;
-  if ( currentClass ) {
-    cube.classList.remove( currentClass );
-  }
-  cube.classList.add( showClass );
-  currentClass = showClass;
+
+var triangle = document.querySelector('.triangle');
+var triangle2 = document.querySelector('.triangle2');
+var cube = document.querySelector('.cube')
+let body = document.body;
+var right = document.querySelector('.right');
+
+function rotateLeft() {
+    if (cube.classList.contains("rotateright")) {
+        cube.classList.remove("rotateright");
+        cube.classList.add("center");
+      } else if (cube.classList.contains("center")) {
+        cube.classList.remove("center")
+        cube.classList.add("rotateleft") 
+      } else {cube.classList.add("rotateleft") 
+    }
+
 }
-// set initial side
-changeSide();
 
-radioGroup.addEventListener( 'change', changeSide );
+function rotateRight() {
+    if (cube.classList.contains("rotateleft")) {
+        cube.classList.remove("rotateleft");
+        cube.classList.add("center");
+      } else if (cube.classList.contains("center")) {
+        cube.classList.remove("center");
+        cube.classList.add("rotateright")
+      } else {
+        cube.classList.add("rotateright");
+      }
+}
+
+
+
+triangle.addEventListener("click", rotateLeft);
+triangle2.addEventListener("click", rotateRight)
