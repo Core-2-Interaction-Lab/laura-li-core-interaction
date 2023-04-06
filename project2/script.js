5b0c7b6622a148ff9bf7b374a3c0c734835e0412
layurathe <laura.li_li@outlook.com>
2023-04-06 04:21:37 -0400

    proj2

--- project2/script.js
+++ project2/script.js
@@ -1,81 +1,66 @@
-const createCollection = (collection) =>{
+const createCollection = (collection) => {
     const collectionGrid = document.getElementById('collection');
-
-    collection.forEach (item => {
-        const listItem = document.createElement('ul')
-
-        collectionGrid.appendChild(listItem)
-
-        // const itemFilm = document.createElement ('h2')
-        // itemFilm.innerHTML = item.Film
-        // listItem.appendChild(itemFilm)
-
-        const itemImage = document.createElement ('img') 
-        if (item.ObjImg) {
-            itemImage.src = item.ObjImg
-            listItem.appendChild(itemImage)
-        }
-
-        // const itemButton = document.createElement ('button')
-        //     itemButton.innerHTML = item.Film
-        //     listItem.appendChild(itemButton)
-
-        
-            const itemDetails =
-			`
-            <div class="description">
-				<div class="highlight">
-                    <div class="object">
-                        <p class="text"> Object: <span>${item.Object}<span></p>
-                    </div>
-					<div class="des">
-                        <p class="text">Film: <span>${item.Film}<span></p>
-                        <p class="text">Date: <span>${item.Date}<span></p>
-                        <p class="text">Created by/ Director: <span>${item.CreatedbyDirector}<span></p>  
-                    </div>
-					
-				</div>
-            <div>
-				
-			`
-		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+  
+    collection.forEach((item) => {
+      const listItem = document.createElement('ul');
+  
+      collectionGrid.appendChild(listItem);
+  
+      const itemImage = document.createElement('img');
+      if (item.ObjImg) {
+        itemImage.src = item.ObjImg;
+        listItem.appendChild(itemImage);
+      }
+  
+      const itemDetails = `
+        <div class="description" style="display:block">
+          <div class="highlight">
+            <div class="exit"> 
+            <p> click to hide </p> 
+            </div>
+            <div class="object">
+              <p class="text"> Object: <span>${item.Object}<span></p>
+            </div>
+            <div class="image">
+              <img src="${item.ObjImg}">
+            </div>
+            <div class="des">
+              <p class="text">Film: <span>${item.Film}<span></p>
+              <p class="text">Date: <span>${item.Date}<span></p>
+              <p class="text">Created by/Director: <span>${item.CreatedbyDirector}<span></p>  
+              <p class="text">Genre: <span>${item.Genre}<span></p>
+            </div>
+          </div>
+        </div>
+      `;
+  
+      listItem.insertAdjacentHTML('beforeend', itemDetails);
+  
+      const highlight = listItem.querySelector('.highlight');
+      const image = listItem.querySelector('img');
+      const title = document.querySelector('.collectiontitle');
+  
+      image.addEventListener('click', () => {
+        const allHighlights = document.querySelectorAll('.highlight');
+        allHighlights.forEach((i) => {
+          i.style.display = 'none';
+        });
+  
+        highlight.style.display = 'block';
+        window.location.href = '#top';
+        title.style.display = 'none';
+      });
+  
+      highlight.addEventListener('click', () => {
+        highlight.style.display = 'none';
+        title.style.display = 'block';
+      });
     });
-    // const createDescription = document.createElement ('description')
-    let button = document.button;
-
-    function log(){
-        console.log("hello")
-    }
-    
-    button.addEventListener ("click", log)
-
-}
-// var objectname = document.querySelector('.objectname');
-// // var img = document.querySelector('ObjImg');
-// let ul = document.ul;
-
-// function log(){
-//     console.timeLog
-// }
-
-// ul.addEventListener("click", log);
-
-// function toggle() {
-//     if (objectname)
-
-// }
-
-
-// objectname.addEventListener ("click", toggle);
-
-// addEventListener(click.createDescription)
-
-
-
-
-fetch('collection.json')
-	.then(response => response.json())
-	.then(collection => {
-		// And passes the data to the function, above!
-		createCollection(collection) // In reverse order
-})
+  };
+  
+  fetch('collection.json')
+    .then((response) => response.json())
+    .then((collection) => {
+      createCollection(collection);
+    });
+  
