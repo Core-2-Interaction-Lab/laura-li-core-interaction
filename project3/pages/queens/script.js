95358ff9e3d0fc498adfa3442c317c18d79f1fbf
layurathe <laura.li_li@outlook.com>
2023-05-09 08:09:26 -0400

    proj3

---
+++ project3/pages/queens/script.js
@@ -0,0 +1,66 @@
+const createNames = (names) => {
+    const nameList = document.getElementById('allnames');
+  
+    names.forEach((item) => {
+        if (item.type_name.includes('Snack Bar')) {
+            const nameEntry = document.createElement('ul');
+            nameList.appendChild(nameEntry);
+    
+            const details = `
+                <div class="nameentry">
+                    <li><span>${item.name}</span></li>
+                </div>
+            `;
+    
+            nameEntry.insertAdjacentHTML('beforeend', details);
+        }
+    });
+};
+
+const createData = (data) => {
+    const dataViz = document.getElementsByClassName('commonnames')[0]; // Use [0] to access the first element of the collection
+
+    data.forEach((item) => {
+        const topThree = document.createElement('div');
+        topThree.className = 'bluesection'; // Add a class name to the created element
+        topThree.innerHTML = '<h1></h1>'; // Set the content of the created element
+
+        dataViz.appendChild(topThree);
+    });
+};
+
+fetch('../../eateries.json')
+    .then((response) => response.json())
+    .then((names) => {
+        createNames(names);
+    });
+
+console.log("hello");
+
+const showAll = document.querySelector('.tab1');
+
+showAll.addEventListener('click', () => {
+    const names = document.querySelectorAll('.nameentry');
+
+    names.forEach(name => {
+        if (name.style.display === "none") {
+            name.style.display = "block";
+        } else {
+            name.style.display = "none";
+        }
+    });
+});
+
+const about = document.querySelector('.tab3');
+
+about.addEventListener('click', () => {
+    const names = document.querySelectorAll('.about');
+
+    names.forEach(name => {
+        if (name.style.display === "none") {
+            name.style.display = "block";
+        } else {
+            name.style.display = "none";
+        }
+    });
+});
--- project3/script.js
+++ project3/script.js
@@ -1,29 +1,41 @@
-const createCollection = (collection) => {
-    const collectionGrid = document.getElementById('fullCollection');
+// const createCollection = (collection) => {
+//     const collectionGrid = document.getElementById('fullCollection');
   
-    collection.forEach((item) => {
-      const listItem = document.createElement('ul');
+//     collection.forEach((item) => {
+//       const listItem = document.createElement('ul');
   
-      collectionGrid.appendChild(listItem);
+//       collectionGrid.appendChild(listItem);
 
-      const itemDetails = `
-        <div class="name">
-            <p><span>${item.name}<span><p>
-        <div>
-      `;
+//       const itemDetails = `
+//         <div class="name">
+//             <p><span>${item.name}<span><p>
+//         <div>
+//       `;
                                         
-      listItem.insertAdjacentHTML('beforeend', itemDetails);
+//       listItem.insertAdjacentHTML('beforeend', itemDetails);
 
 
-    });
-  };
+//     });
+//   };
 
-  // const 
+
+
+
   
-  fetch('eateries.json')
-    .then((response) => response.json())
-    .then((collection) => {
-      createCollection(collection);
-    })
-    console.log("hello");
+//   fetch('eateries.json')
+//     .then((response) => response.json())
+//     .then((collection) => {
+//       createCollection(collection);
+//     })
+//     console.log("hello");
   
+
+window.addEventListener('DOMContentLoaded', function() {
+  // const container = document.querySelector('.container');
+  // const screenHeight = window.innerHeight;
+  // const containerHeight = container.clientHeight;
+
+  // const scrollToMiddle = (screenHeight - containerHeight) / 2;
+  window.scrollTo(0, 3600);
+});
+
