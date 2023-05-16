0806ec83e32951893c321c68804c636f0fc4bd66
layurathe <laura.li_li@outlook.com>
2023-05-15 21:58:08 -0400

    proj2

--- project2/script.js
+++ project2/script.js
@@ -5,14 +5,28 @@ const createCollection = (collection) => {
       const listItem = document.createElement('ul');
   
       collectionGrid.appendChild(listItem);
+
+      const input = document.createElement('input');
+      
   
       const itemImage = document.createElement('img');
+      itemImage.classList.add("cutout");
+      itemImage.style.display = 'none';
       if (item.ObjImg) {
         itemImage.src = item.ObjImg;
         listItem.appendChild(itemImage);
       }
+
+      const itemBlack = document.createElement('img');
+      itemBlack.classList.add("blackout");
+      itemBlack.style.display = 'block';
+      if (item.imgBlack) {
+        itemBlack.src = item.imgBlack;
+        listItem.appendChild(itemBlack);
+      }
   
       const itemDetails = `
+        <input type ="Guess" placeholder = "enter guess">
         <div class="description" style="display:block">
           <div class="highlight">
             <div class="exit"> 
@@ -21,9 +35,7 @@ const createCollection = (collection) => {
             <div class="object">
               <p class="text"> Object: <span>${item.Object}<span></p>
             </div>
-            <div class="image">
-              <img src="${item.ObjImg}">
-            </div>
+
             <div class="des">
               <p class="text">Film: <span>${item.Film}<span></p>
               <p class="text">Date: <span>${item.Date}<span></p>
@@ -32,8 +44,13 @@ const createCollection = (collection) => {
             </div>
           </div>
         </div>
+        
       `;
   
+    //   <div class="image">
+    //   <img src="${item.ObjImg}">
+    // </div>      
+
       listItem.insertAdjacentHTML('beforeend', itemDetails);
   
       const highlight = listItem.querySelector('.highlight');
@@ -48,7 +65,7 @@ const createCollection = (collection) => {
         });
   
         highlight.style.display = 'block';
-        window.location.href = '#top';
+        // window.location.href = '#top';
         title.style.display = 'none';
         challenge.style.display = 'none';
 
@@ -60,6 +77,42 @@ const createCollection = (collection) => {
         challenge.style.display = 'block';
 
       });
+      const easy = document.querySelector('.easy');
+      const hard = document.querySelector('.hard');
+      const cutout = document.querySelectorAll('.cutout');
+      const blackout = document.querySelectorAll('.blackout');
+
+      easy.addEventListener('click', () => {
+        if (hard.style.backgroundColor = "#F5D9D") {
+          hard.removeAttribute('style');
+          easy.style.backgroundColor='#FB7F7F';
+        } else {
+          easy.style.backgroundColor = "#F5D9D"
+        }
+        cutout.forEach((element) => {
+          element.style.display = 'block';
+        });
+        blackout.forEach((element) => {
+          element.style.display = 'none';
+        });
+      });
+
+      hard.addEventListener('click', () => {
+        if (easy.style.backgroundColor = "#FB7F7F") {
+          easy.removeAttribute('style');
+          hard.style.backgroundColor='#FB7F7F';
+        } else {
+          easy.style.backgroundColor = "#F5D9D"
+        }
+        blackout.forEach((element) => {
+          element.style.display = 'block';
+        });
+        cutout.forEach((element) => {
+          element.style.display = 'none';
+        });
+      });
+      
+
     });
   };
   
